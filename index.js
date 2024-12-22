3cc54fc2209e530133c1d0320e58ef88e6a979e2
precious <poppyszn.dev@gmail.com>
2024-12-22 21:27:38 +0100

    added dd-agent module

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+const tracer = require('dd-trace');
+const formats = require('dd-trace/ext/formats');
+
+class Logger {
+    log(level, message) {
+        const span = tracer.scope().active();
+        const time = new Date().toISOString();
+        const record = { time, level, message };
+
+        if (span) {
+            tracer.inject(span.context(), formats.LOG, record);
+        }
+
+        console.log(JSON.stringify(record));
+    }
+}
+
+module.exports = Logger;
 
 // Middleware to parse JSON payloads if needed
 app.use(express.json());
