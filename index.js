43d5604ccba29dc67d2dc1d9044aa148b48a3db8
precious <poppyszn.dev@gmail.com>
2024-12-22 21:55:39 +0100

    editied index

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const app = express();
-const tracer = require('dd-trace');
-const formats = require('dd-trace/ext/formats');
-
-class Logger {
-    log(level, message) {
-        const span = tracer.scope().active();
-        const time = new Date().toISOString();
-        const record = { time, level, message };
-
-        if (span) {
-            tracer.inject(span.context(), formats.LOG, record);
-        }
-
-        console.log(JSON.stringify(record));
-    }
-}
-
-module.exports = Logger;
+const tracer = require('dd-trace').init({
+  logInjection: true
+});
 
 // Middleware to parse JSON payloads if needed
 app.use(express.json());
